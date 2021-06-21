1b0aa800b17b035a073d23d27fcac2a3e60bc1ea
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2021-06-21 19:21:08 +0100

    Create stoplogwatcher.sh

---
+++ stafi-startstop/stoplogwatcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# gracefully stop node
+collect_pid=$(pgrep restorelogwatcher)
+
+if [ ! -z "$collect_pid" ]; then
+        kill -SIGINT $collect_pid
+echo "kill restorelogwatcher"
+else
+echo "restorelogwatcher not running"
+fi
