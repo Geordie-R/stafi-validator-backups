c5a42dc84d8f746be87560dd81f1a37f5dc609e2
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2021-07-18 00:08:36 +0100

    Update newtakesnapshot.sh

--- stafi-backups/newtakesnapshot.sh
+++ stafi-backups/newtakesnapshot.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #if [[ $UID != 0 ]];
@@ -16,53 +15,67 @@ get_config_value "platform-dir"
 platformdir=$(echo $global_value)
 
 
-sshfile="$HOME/.ssh/id_rsa"
-sshportno=22
-remote_user="your-ubuntu-user@your-server-host.com"
-remote_server_folder="/var/www/remotewebsite.co.uk/public_html/"
+sshfile="$HOME/.ssh/id_stafi"
+sshportno=468
+remote_user="youruser@yourwebspace.co.uk"
+remote_server_folder="/var/www/yourwebspace.co.uk/public_html"
 downloads_folder="$HOME/geordiertools/downloads"
 datename=$(date +%Y-%m-%d_%H-%M)
-filename="$downloads_folder/$datename-blocks"
+filename="$downloads_folder/$datename-db-"
 shcreate="$HOME/geordiertools/send-snapshot.sh"
 
 #Remove older files just leave last couple
 echo "Removing old backups"
 ls -F $downloads_folder/*.gz | head -n -1 | xargs -r rm
-ls -F $downloads_folder/*.json | head -n -1 | xargs -r rm
+#ls -F $downloads_folder/*.json | head -n -1 | xargs -r rm
 
 dqt='"' # Store double quote for easy escaping
 
-#rm -rf $shcreate
-echo "Exporting blocks.json on $datename..."
+rm -rf $shcreate
+echo "Exporting db directory on $datename..."
 
-$HOME/geordiertools/stafi-startstop/stop.sh
+sudo $HOME/geordiertools/stafi-startstop/stop.sh
 sleep 1
 
 cd "$HOME/$platformdir/"
 #./target/release/stafi export-blocks --pruning archive > $filename.json
 
-
-
-
-
-#echo "HELLO WORLDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD123D" >> $filename.json
-
-
+dir_compress="$HOME/.local/share/stafi/chains/stafi_mainnet/db/"
+dir_size=$(du -sb $dir_compress | awk '{print $1}')
+filename="$downloads_folder/$datename-db-$dir_size"
 if [[ $1 == "send" ]];
 then
-echo "Creating send-snapshot.sh..."
-echo "tar -Scvzf $filename.tar.gz ~/.local/share/stafi/chains/stafi_mainnet/db" >> $shcreate
-echo "ssh -p $sshportno -i '$sshfile' $remote_user 'find $remote_server_folder -name ${dqt}*.gz${dqt} -type f -size -1k -delete'" >> $shcreate
-echo "ssh -p $sshportno -i '$sshfile' $remote_user 'ls -F $remote_server_folder*.gz | head -n -1 | xargs -r rm'" >> $shcreate
-echo "rsync -rv -e 'ssh -p $sshportno' -i ${dqt}$sshfile${dqt} --progress $filename.tar.gz $remote_user:$remote_server_folder" >> $shcreate
+echo "" >> $shcreate
+echo "Creating compressed archive..."
+echo "cd $HOME/.local/share/stafi/chains/stafi_mainnet/db/" >> $shcreate
+echo "tar -Scf - * | pv -s $dir_size | pigz -3 -p 4 > $filename.tar.gz" >> $shcreate
+
+#echo "sudo pigz -Scf - $filename.tar.gz | pv -s $dir_size | tar cf -" >> $shcreate
+
+
+
+
+# example extraction for above pv work     sudo pigz -dc $downloadsdir/$latest_file | pv -s $(($(du -sb $downloadsdir/$latest_file | awk '{print $1}') * 1)) | tar xf -
+
+
+echo "ssh -p $sshportno -i '$sshfile' $remote_user 'find $remote_server_folder/ -name ${dqt}*.gz${dqt} -type f -size -1k -delete'" >> $shcreate
+#echo "ssh -p $sshportno -i '$sshfile' $remote_user 'ls -F $remote_server_folder/*.gz | head -n -1 | xargs -r rm'" >> $shcreate
+echo "ssh -p $sshportno -i '$sshfile' $remote_user 'ls -F $remote_server_folder/*.gz | xargs -r rm'" >> $shcreate #Remove all for now due to space issues
+echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename.tar.gz $remote_user:$remote_server_folder/" >> $shcreate
+
+####################echo "rsync -rv -e 'ssh -p $sshportno' -i '$sshfile' --progress $filename.tar.gz $remote_user:$remote_server_folder/" >> $shcreate
+
+
+
 
 
 chmod +x $shcreate
 echo "Sending the backup...`date`"
 $shcreate
+cd "$HOME/$platformdir/"
 echo "finished sending `date`"
 sleep 1
-#rm $shcreate
+
 fi
 
 echo "Finished take-snapshot.sh at $datename"
