288be5535d870eff8e755ef9e04c75fd573e8bd0
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2022-06-01 23:10:06 +0100

    Update purge-wipe.sh

--- stafi-restores/purge-wipe.sh
+++ stafi-restores/purge-wipe.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-. $HOME/geordiertools/useful_functions.sh
+. $home_var/geordiertools/useful_functions.sh
 get_config_value "validator-name"
 validatorname=$(echo $global_value)
 
 get_config_value "platform-dir"
 platformdir=$(echo $global_value)
 
-log_file=$HOME/$platformdir/$platformdir.log
+log_file=$home_var/$platformdir/$platformdir.log
 
 
 
 echo "Running purge of database"
-cd "$HOME/$platformdir"
+cd "$home_var/$platformdir"
 
 ./target/release/stafi purge-chain --chain=mainnet
