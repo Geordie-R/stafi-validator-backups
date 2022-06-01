867d3970e06924a9aee0f96a6d07f60150908eff
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2022-06-01 23:13:50 +0100

    Update clearlog.sh

--- stafi-startstop/clearlog.sh
+++ stafi-startstop/clearlog.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-. $HOME/geordiertools/useful_functions.sh
+
+home_var="/home/stafi"
+
+. $home_var/geordiertools/useful_functions.sh
 get_config_value "platform-dir"
 platformdir=$(echo $global_value)
-log_file=$HOME/$platformdir/$platformdir.log
+log_file=$home_var/$platformdir/$platformdir.log
 #Empty the log file
 > $log_file
